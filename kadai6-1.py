234edba67efe7276c92662a12f6a2fc94ef4bd87
j21083岡本哲弥 <j21083ot@edu.tuis.ac.jp>
2025-06-15 21:32:00 +0900

    Update kadai6-1.pyコメントを追加しました。

--- kadai6-1.py
+++ kadai6-1.py
@@ -1,6 +1,17 @@
 import requests
 import json
 
+# ---------------------------------------------------------------------
+# このプログラムは 教育統計調査データを取得します。
+# 対象データID：000040277626（教育統計調査 学校基本調査）
+# APIのエンドポイント：https://api.e-stat.go.jp/rest/3.0/app/json/getStatsData
+# 利用機能：getStatsData（統計データ取得）
+
+# ---------------------------------------------------------------------
+
+mport requests
+import json
+
 APP_ID = "aed1b1fc27cc6e970968a017cc49d15c2a73cfa8"
 
 API_URL = "https://api.e-stat.go.jp/rest/3.0/app/json/getStatsList"
